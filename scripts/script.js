ab1caf15c1a05f65b4448fcd55ecf0ff95899d9b
Carlos Gonzalez <carlossgv@gmail.com>
2021-02-14 10:14:21 -0300

    funcionality done, styling to do

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // Module object for gameBoard and displayController
 const gameBoard = (() => {
-  const gameboard = ['', '', 'X', 'O', 'O', 'X', 'X', 'O', 'O'];
+  const gameboard = ['', '', '', '', '', '', '', ''];
 
   return {
     gameboard,
@@ -14,8 +14,67 @@ const player = (name, mark, place) => {
 };
 
 // Object to control the flow of the game (?)
+let select_buttons = document.querySelectorAll('.select-button');
 
-const gamePlay = () => {};
+select_buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (
+      document.getElementById('name1').value === '' ||
+      document.getElementById('name2').value === ''
+    ) {
+      alert('Please enter players names');
+    } else {
+      gamePlay(gameBoard.gameboard, e.target.innerHTML);
+    }
+  });
+});
+
+const gamePlay = (gameboard, mark) => {
+  const squares = document.querySelectorAll('.square');
+
+  const players = selectPlayer(mark);
+
+  const player1 = players.player1;
+  const player2 = players.player2;
+
+  mark = player1.mark;
+  console.log(player1.mark);
+
+  const play = ((mark) => {
+    squares.forEach((square) => {
+      square.addEventListener('click', (square) => {
+        if (!checkWinner(gameboard)) {
+          addMark(square, mark);
+
+          if (checkWinner(gameboard).finish) {
+            if (mark === player1.mark) {
+              return finishGame(player1, checkWinner(gameboard).type);
+            } else {
+              return finishGame(player2, checkWinner(gameboard).type);
+            }
+          }
+
+          if (mark === 'X') {
+            mark = 'O';
+          } else {
+            mark = 'X';
+          }
+        }
+      });
+    });
+  })(mark);
+
+  const finishGame = (player, type) => {
+    if (type === 'tie') {
+      document.querySelector('.result').innerHTML =
+        'It was a tie! Want to play again?';
+    } else {
+      document.querySelector('.result').innerHTML =
+      `${player.name} won! Want to play again?`;
+    }
+    document.querySelector('.result-div').style.display = 'block';
+  };
+};
 
 // ===============================================
 function fillBoard() {
@@ -24,14 +83,17 @@ function fillBoard() {
   }
 }
 
-let select_buttons = document.querySelectorAll('.select-button');
+// select_buttons.forEach((button) => {
+//   button.addEventListener('click', (e) => {
+//     let players = selectPlayer(e.target.innerHTML)
+//     return players
+//   });
+// });
 
-select_buttons.forEach((button) => {
-  button.addEventListener('click', (e) => selectPlayer(e.target.innerHTML));
-})
-function selectPlayer(mark1) {
-    const name1 = document.querySelector('#name1').value
-    const name2 = document.querySelector('#name2').value
+const selectPlayer = (mark1) => {
+  const name1 = document.querySelector('#name1').value;
+  const name2 = document.querySelector('#name2').value;
+
   let mark2 = '';
   if (mark1 === 'X') {
     mark2 = 'O';
@@ -39,30 +101,31 @@ function selectPlayer(mark1) {
     mark2 = 'X';
   }
 
-  const player1 = player(name1, mark1, 1)
-  const player2 = player(name2, mark2, 2)
+  const player1 = player(name1, mark1, 1);
+  const player2 = player(name2, mark2, 2);
 
-  document.getElementById('player1').innerHTML = `${player1.name} is ${player1.mark}`
-  document.getElementById('player2').innerHTML = `${player2.name} is ${player2.mark}`
+  document.getElementById(
+    'player-1'
+  ).innerHTML = `${player1.name} is ${player1.mark}`;
+  document.getElementById(
+    'player-2'
+  ).innerHTML = `${player2.name} is ${player2.mark}`;
 
   document.querySelector('.player-select').style.display = 'None';
 
-  console.log(player1, player2)
-}
+  console.log(player1, player2);
+  return { player1: player1, player2: player2 };
+};
 
-const squares = document.querySelectorAll('.square');
+const addMark = (e, mark) => {
+  const square = e.target;
 
-squares.forEach((square) => {
-  square.addEventListener('click', (square) => addMark(square));
-});
-
-function addMark(e) {
-  const mark = e.target;
-
-  if (!mark.innerHTML) {
-    console.log(mark);
+  if (!square.innerHTML) {
+    console.log(square);
+    square.innerHTML = mark;
+    gameBoard.gameboard[square.id] = mark;
   }
-}
+};
 
 function checkWinner(gameboard) {
   const v0 = gameboard[0];
@@ -76,17 +139,30 @@ function checkWinner(gameboard) {
   const v8 = gameboard[8];
 
   if (
-    (v0 === v1 && v1 === v2) ||
-    (v0 === v3 && v3 === v6) ||
-    (v0 === v4 && v4 === v8) ||
-    (v3 === v4 && v4 === v5) ||
-    (v1 === v4 && v4 === v7) ||
-    (v2 === v4 && v4 === v6) ||
-    (v6 === v7 && v7 === v8) ||
-    (v2 === v5 && v5 === v8)
+    (v0 === v1 && v1 === v2 && v0 != '') ||
+    (v0 === v3 && v3 === v6 && v0 != '') ||
+    (v0 === v4 && v4 === v8 && v0 != '') ||
+    (v3 === v4 && v4 === v5 && v3 != '') ||
+    (v1 === v4 && v4 === v7 && v1 != '') ||
+    (v2 === v4 && v4 === v6 && v2 != '') ||
+    (v6 === v7 && v7 === v8 && v6 != '') ||
+    (v2 === v5 && v5 === v8 && v2 != '')
   ) {
-    console.log('winner');
+    return { finish: true, type: 'win' };
+  } else if (boardFull(gameboard)) {
+    return { finish: true, type: 'tie' };
+  } else {
+    return false;
   }
+}
+
+function boardFull(gameboard) {
+  for (var i = 0, l = gameboard.length; i < l; i++) {
+    if (gameboard[i] === '') {
+      return false;
+    }
+  }
+  return true;
 }
 
 fillBoard();
